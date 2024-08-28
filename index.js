f7f5410bf0ffaeeca1d7036102f83aed1b4fea4a
Vojin Mitrovic <vojin.mitrovic@gmail.com>
2024-08-28 13:41:42 +0200

    111

--- index.js
+++ index.js
@@ -22,3 +22,29 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+// dark mode implementation
+
+const toggleButton = document.getElementById('dark-mode-toggle');
+const rootElement = document.documentElement; // or document.body
+const logoImage = document.getElementById('logo-image');
+
+// Load saved theme preference
+if (localStorage.getItem('theme') === 'dark') {
+    rootElement.classList.add('dark-mode');
+    logoImage.src = "assets/safervault-logo-v2-bright.png";
+    toggleButton.textContent = "Light theme";
+}
+
+toggleButton.addEventListener('click', () => {
+    if (rootElement.classList.toggle('dark-mode')) {
+        logoImage.src = "assets/safervault-logo-v2-bright.png";
+        toggleButton.textContent = "Light theme";
+        localStorage.setItem('theme', 'dark');
+    } else {
+        logoImage.src = "assets/safervault-logo-v2-dark.png";
+        toggleButton.textContent = "Dark theme";
+        localStorage.setItem('theme', 'light');
+    }
+});
